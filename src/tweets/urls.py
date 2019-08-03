24d168b3a565732b3a6feaf309a74fbd43d711dd
Richard <starengram@gmail.com>
2019-08-03 15:28:41 +0800

    class as view queryset must lowercase

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,9 +1,9 @@
 
 from django.urls import path
-from .views import tweet_detail_view, tweet_list_view
+from .views import tweet_detail_view, tweet_list_view, TweetListView, TweetDetailView
 
 urlpatterns = [
-    path('', tweet_list_view, name='list'),
-    path('2', tweet_detail_view, name='detail'),
+    path('', TweetListView.as_view(), name='list'),
+    path('2', TweetDetailView.as_view(), name='detail'),
 
 ]
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
 from .models import Tweet
 
 # Create your views here.
+
+class TweetDetailView(DetailView):
+    #template_name = "tweets/detail_view.html"
+    queryset = Tweet.objects.all()
+    #model = Tweet
+
+    def get_object(self):
+        Tweet.objects.get(id=2)
+
+class TweetListView(ListView):
+    #template_name = "tweets/list_view.html"
+    queryset = Tweet.objects.all()
+    #model = Tweet
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+
 
 def tweet_detail_view(request, id=2):
     obj = Tweet.objects.get(id=id)
