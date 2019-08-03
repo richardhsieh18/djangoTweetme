d6c7fb391832bad765027362af8ec0d104c7e1b6
Richard <starengram@gmail.com>
2019-08-03 14:39:30 +0800

    render out

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 
+from .models import Tweet
+
 # Create your views here.
 
 def tweet_detail_view(request, id=2):
-    return render(request, "tweets/tweet_detail_view.html", {})
+    obj = Tweet.objects.get(id=id)
+    print(obj)
+    context = {
+        "object": obj,
+        "testdata": obj
+    }
+    return render(request, "tweets/detail_view.html", context)
 
-def tweet_list_view(request, id=2):
-    return render(request, "tweets/list_view.html", {})
+def tweet_list_view(request):
+    querySet = Tweet.objects.all()
+    print(querySet)
+    for obj in querySet:
+        print(obj.content)
+    context = {
+        "object_list": querySet
+    }
+    return render(request, "tweets/list_view.html", context)
