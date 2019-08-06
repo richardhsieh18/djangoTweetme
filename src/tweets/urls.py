eac557b6b1c30056979a1fa9de2405687166446a
Richard <starengram@gmail.com>
2019-08-06 18:06:13 +0800

    CreateView

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,10 +1,14 @@
 
 from django.conf.urls import url
-from .views import TweetListView, TweetDetailView
+from .views import (
+    TweetCreateView,
+    TweetListView, 
+    TweetDetailView
+    )
 
 urlpatterns = [
     url(r'^$', TweetListView.as_view(), name='list'),
-    #url(r'^(?P<pk>\d+)/$', tweet_detail_view, name="detail"),
+    url(r'^create/$', TweetCreateView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name="detail"),
 
 ]
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView
 
 from .models import Tweet
+from .forms import TweetModelForm
 
 # Create your views here.
+
+class TweetCreateView(CreateView):
+    form_class = TweetModelForm
+    template_name = "tweets/create_view.html"
+    success_url = '/tweet/create/'
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TweetCreateView, self).form_valid(form)
+
+def tweet_create_view(request):
+    form = TweetModelForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.user = request.user
+        instance.save()
+    context = {
+        'form': form
+    }
+    return render(request, "tweets/create_view.html", context)
+
 
 class TweetDetailView(DetailView):
     #template_name = "tweets/detail_view.html"
