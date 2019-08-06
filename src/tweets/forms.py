01af436f442bd135febfd2609d1c931599a84f32
Richard <starengram@gmail.com>
2019-08-06 13:44:29 +0800

    add form model

---
+++ src/tweets/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from .models import Tweet
+
+
+class TweetModelForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = [
+            "user",
+            "content"
+        ]
+
+    def clean_content(self, *args, **kwargs):
+        content = self.cleaned_data["content"]
+        if content == "abc":
+            raise forms.ValidationError("Cannot be ABC")
+        return content
+    
