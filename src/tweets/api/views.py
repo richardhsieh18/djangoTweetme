e3b508d6bab58ebe0c1e609eb72bb37c1ba3547f
Richard <starengram@gmail.com>
2019-08-14 14:09:20 +0800

    jquery search p38

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from django.db.models import Q
 
 from tweets.models import Tweet
 from .serializers import TweetModelSerialzer
@@ -6,6 +7,14 @@ from .serializers import TweetModelSerialzer
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerialzer
 
-    def get_queryset(self):
-        return Tweet.objects.all()
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        print(self.request.GET)
+        query = self.request.GET.get('q', None)
+        if query is not None:
+            qs = qs.filter(
+                Q(content__icontains=query) |
+                Q(user__username__icontains=query)
+                )
+        return qs
     
