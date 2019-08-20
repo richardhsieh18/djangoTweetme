b655637057990aac37ca6d638678b06e8ac12028
Richard <starengram@gmail.com>
2019-08-20 12:11:31 +0800

    get follower count

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -2,9 +2,27 @@ from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class UserProfileManager(models.Manager):
+    use_for_related_fields = True
+    def all(self):
+        qs = self.get_queryset().all()
+        try:
+            if self.instance:
+                qs = qs.exclude(user=self.instance)
+        except:
+            pass
+        return qs
+
+
 class UserProfile(models.Model):
     user        = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile', on_delete=models.CASCADE)
     following   = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')
 
+    objects = UserProfileManager() #UserProfile.objects.all()
+
     def __str__(self):
         return str(self.following.all().count())
+    
+    def get_following(self):
+        users = self.following.all() #User.objects.all().exclude(username=self.user.username)
+        return users.exclude(username=self.user.username)
