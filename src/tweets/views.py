eb38788352bdb1ecdaaedca3471dd63423b88798
Richard <starengram@gmail.com>
2019-08-22 19:06:58 +0800

    retweet Part1

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,12 +1,22 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy, reverse
 from django.db.models import Q
 
 from .models import Tweet
 from .mixins import FormUserNeededMixin, UserOwnerMixin
 from .forms import TweetModelForm
+
+class RetweetView(View):
+    def get(self, request, pk, *args, **kwargs):
+        tweet = get_object_or_404(Tweet, pk=pk)
+        if request.user.is_authenticated:
+            new_tweet = Tweet.objects.retweet(request.user, tweet)
+            return HttpResponseRedirect("/")
+        return HttpResponseRedirect(tweet.get_absolute_url())
 
 class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
