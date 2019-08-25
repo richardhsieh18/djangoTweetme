4e32d2fd6c411017be4e327766b48052348df53e
Richard <starengram@gmail.com>
2019-08-26 00:54:36 +0800

    single detailtweet

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -6,6 +6,7 @@ from .views import (
     RetweetAPIView,
     TweetListAPIView,
     TweetCreateAPIView,
+    TweetDetailAPIView,
     )
 
 app_name = 'tweet'
@@ -17,6 +18,7 @@ urlpatterns = [
     url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), #/tweet/create/
     url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name="like-toggle"), #/api/tweet/id/like/
     url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name="retweet"), #/api/tweet/id/retweet/
+    url(r'^(?P<pk>\d+)/$', TweetDetailAPIView.as_view(), name="detail"),
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name="detail"), #/tweet/1/
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name="update"), #/tweet/1/update/
     # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name="delete"), #/tweet/1/delete/
--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -42,6 +42,18 @@ class TweetCreateAPIView(generics.CreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+class TweetDetailAPIView(generics.ListAPIView):
+    queryset = Tweet.objects.all()
+    serializer_class = TweetModelSerialzer
+    pagination_class = StandardResultsPagination
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self, *args, **kwargs):
+        tweet_id = self.kwargs.get("pk")
+        qs = Tweet.objects.filter(pk=tweet_id)
+        return qs
+        
+
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerialzer
     pagination_class = StandardResultsPagination
